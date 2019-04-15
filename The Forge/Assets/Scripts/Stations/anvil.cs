76c4a68af8cecfe5bed35432c8a65c48e05d7a69
Zixuan Zou <zouzixuan1117@gmail.com>
2019-04-15 12:16:03 -0400

    anvil minigame

--- The Forge/Assets/Scripts/Stations/anvil.cs
+++ The Forge/Assets/Scripts/Stations/anvil.cs
@@ -2,11 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class anvil : MonoStation {
-	public override void on_interact(player Player) {
-		base.on_interact(Player);
-		sound_manager.play_one_shot(sound_manager.instance.anvil_hit);
-		// todo - play minigame
-		complete_items_swap();
-	}
+public class anvil : MonoStation
+{
+    private int count = 0;
+    private bool is_playing = false;
+
+    private int player_num;
+    public int button_mash_num;
+
+    public SpriteRenderer bar_location;
+    public spritesheet bar_sprites;
+    private int progress_num = 10;
+
+    public override void on_interact(player Player)
+    {
+        base.on_interact(Player);
+        sound_manager.play_one_shot(sound_manager.instance.anvil_hit);
+        player_num = Player.index;
+        is_playing = true;
+
+    }
+
+    private void Start()
+    {
+        bar_location.enabled = false;
+        count = 0;
+    }
+
+    // Called every frame
+    protected override void Update()
+    {
+        base.Update();
+        // Your stuff here
+        if (input.p[player_num].interact && is_playing)
+        {
+            count++;
+            Debug.Log("count = " + count);
+        }
+
+        if (count == button_mash_num && is_playing)
+        {
+            complete_items_swap();
+            count = 0;
+            is_playing = false;
+            bar_location.sprite = bar_sprites.sprites[0];
+            bar_location.enabled = false;
+        }
+
+        if (is_playing)
+        {
+            bar_location.enabled = true;
+            bar_location.sprite = bar_sprites.sprites[count / (button_mash_num / progress_num)];
+        }
+
+    }
 }
