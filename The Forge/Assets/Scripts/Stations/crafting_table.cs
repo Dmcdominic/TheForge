ba4c263b37039083ec3e590b408a6c36237637f9
Zixuan Zou <zouzixuan1117@gmail.com>
2019-04-26 21:23:40 -0400

    crafting table minigame

--- The Forge/Assets/Scripts/Stations/crafting_table.cs
+++ The Forge/Assets/Scripts/Stations/crafting_table.cs
@@ -2,10 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class crafting_table : MonoStation {
-	public override void on_interact(player Player) {
-		base.on_interact(Player);
-		// todo - play minigame?
-		complete_items_swap();
-	}
+public class crafting_table : MonoStation
+{
+    public GameObject indic;
+    public GameObject bar;
+
+    public bool is_playing = false;
+    public int player_num;
+    public int total_num;
+    public int boost_size;
+
+    private bool is_finished = false;
+
+
+    public override void on_interact(player Player)
+    {
+        base.on_interact(Player);
+        // todo - play minigame?
+        player_num = Player.index;
+        is_playing = true;
+    }
+
+    private void Start()
+    {
+        indic.SetActive(false);
+        bar.SetActive(false);
+        is_playing = false;
+    }
+
+
+    // Called every frame
+    protected override void Update()
+    {
+        base.Update();
+        // Your stuff here
+        is_finished = false;
+
+        if (is_playing)
+        {
+            indic.SetActive(true);
+            bar.SetActive(true);
+            is_finished = GameObject.Find("indicator").GetComponent<crafting_table_indicator>().finished;
+            Debug.Log(is_finished);
+            Debug.Log("been heeeeeeeeere");
+            if (is_finished)
+            {
+                complete_items_swap();
+                is_playing = false;
+                is_finished = false;
+
+            }
+        }
+        else
+        {
+            indic.SetActive(false);
+            bar.SetActive(false);
+        }
+
+    }
+
+
+
 }
+
+
+   
---
+++ The Forge/Assets/Scripts/Stations/crafting_table_indicator.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crafting_table_indicator : MonoBehaviour
+{
+    public GameObject indic;
+    public GameObject bar;
+
+    public GameObject center;
+    public GameObject edge_left;
+    public GameObject edge_right;
+
+    private int total_num;
+    private int boost_size;
+
+    private int direction;
+
+    private int counter;
+    private bool in_center;
+    private int player_num;
+    private bool is_playing;
+
+    public bool finished = false;
+
+    private void Start()
+    {
+        direction = 1;
+        counter = 0;
+        total_num = GameObject.Find("crafting_table").GetComponent<crafting_table>().total_num;
+        player_num = GameObject.Find("crafting_table").GetComponent<crafting_table>().player_num;
+        boost_size = GameObject.Find("crafting_table").GetComponent<crafting_table>().boost_size;
+    }
+
+    void Update()
+    {
+        // Your stuff here
+        finished = false;
+        Debug.Log("beeeeeeeeeeen here");
+
+        indic.transform.position += Vector3.right * direction * Time.deltaTime;
+
+        is_playing = GameObject.Find("crafting_table").GetComponent<crafting_table>().is_playing;
+      
+        if (in_center && input.p[player_num].hand_tool && is_playing)
+        {
+            counter += boost_size;
+
+        }
+        if (is_playing && counter > total_num)
+        {
+
+            counter = 0;
+            finished = true;
+        }
+
+        if (is_playing == false)
+        {
+            counter = 0;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        in_center = false;
+
+        if (((collision.name == "edge_right") || (collision.name == "edge_left")) && is_playing)
+        {
+            direction = -direction;
+            counter += 1;
+        }
+        if ((collision.name == "center"))
+        {
+            in_center = true;
+        }
+
+
+    }
+}
